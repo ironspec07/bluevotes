43306ecfcfa4cc577e8d50bfec64379763618957
anurag <anuragpathakoffl@gmail.com>
2024-05-01 11:20:37 +0530

    made modifications

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,33 +1,34 @@
 require("@nomiclabs/hardhat-waffle");
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   defaultNetwork: "localhost",
   networks: {
     hardhat: {
+      chainId: 31337,
     },
     localhost: {
-      url: "http://127.0.0.1:8545"
+      url: "http://127.0.0.1:8545",
     },
     goerli: {
       url: process.env.ENDPOINT_URL,
-      accounts: [process.env.DEPLOYER_KEY]
-    }
+      accounts: [process.env.DEPLOYER_KEY],
+    },
   },
   solidity: {
-    version: '0.8.11',
+    version: "0.8.11",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
   paths: {
     sources: "./src/contracts",
-    artifacts: "./src/abis"
+    artifacts: "./src/abis",
   },
   mocha: {
-    timeout: 40000
-  }
-}
+    timeout: 40000,
+  },
+};
